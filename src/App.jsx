5214b65f7f58788ad328592029b05f311db0a3f8
woojaemin0106 <dnwoals19@naver.com>
2025-12-03 17:15:05 +0900

    swiper 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,8 @@ import MovieDetail from "./components/MovieDetail.jsx"; // 위치에 맞게
 function App() {
   return (
     <Routes>
-      {/* Layout: NavBar + Outlet */}
       <Route element={<Layout />}>
-        {/* / → 홈(방금 만든 Home) */}
         <Route path="/" element={<Home />} />
-
-        {/* /details → 상세 페이지 */}
         <Route path="/details" element={<MovieDetail />} />
       </Route>
     </Routes>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import movieListData from "./data/movieListData.json";
 import MovieCard from "./components/MovieCard";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 function Home() {
   const [movies] = useState(movieListData.results);
 
@@ -11,18 +12,37 @@ function Home() {
         <h1 className="text-2xl font-bold ">인기 영화</h1>
         <p className="mt-1 text-sm text-slate-400">더미 메인 페이지</p>
       </header>
-
-      {/* MovieCard 그리드 */}
       <main className="px-6 pb-10">
-        <section className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-          {movies.map((movie) => (
-            <MovieCard
-              key={movie.id}
-              title={movie.title}
-              posterPath={movie.poster_path}
-              rating={movie.vote_average}
-            />
-          ))}
+        <section>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={4}
+            breakpoints={{
+              0: {
+                slidesPerView: 1, // 모바일
+              },
+              640: {
+                slidesPerView: 2, // sm
+              },
+              768: {
+                slidesPerView: 3, // md
+              },
+              1024: {
+                slidesPerView: 4, // lg
+              },
+            }}
+          >
+            {movies.map((movie) => (
+              <SwiperSlide>
+                <MovieCard
+                  key={movie.id}
+                  title={movie.title}
+                  posterPath={movie.poster_path}
+                  rating={movie.vote_average}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </section>
       </main>
     </div>
